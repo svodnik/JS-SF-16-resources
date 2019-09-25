29772f9ad792ea3011036f32d32923b716d935be
Sasha Vodnik <sv@sashavodnik.com>
2019-09-25 16:18:09 -0700

    added resources for class 3

---
+++ 03-conditionals-functions/starter-code/Homework-1/random-address-generator/app.js
@@ -0,0 +1,26 @@
+'use strict';
+/*
+  Write code that generates random addresses
+  Each time you load or reload the page in a browser, your program should log
+  a new randomly-generated address to the console.
+
+  Step 1:
+    Create arrays that contain dummy data for each of the following:
+    street number, street name, city name, state name, and zip code
+
+  Step 2:
+    Write code that randomly selects one item from each of these arrays, uses
+    the selected values to construct a string containing a random address, and
+    then logs the result to the console.
+
+  Step 3:
+    Save your work, load your app in the browser, open the console, and verify
+    that a random address is logged.
+
+  Step 4:
+    Reload the page and verify that a different random address is logged.
+
+  Example output:
+    34578 Dolphin Street, Wonka NY, 44506
+*/
+
